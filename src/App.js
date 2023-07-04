29d52dc0ee760412ff885544f924b871e63fa3eb
JeanHaidar <26410714+JeanHaidar@users.noreply.github.com>
2023-07-04 15:48:22 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Chatbot from 'react-chatbot-kit'
+
+import ActionProvider from './ActionProvider';
+import MessageParser from './MessageParser';
+import config from './config';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+    <div className="App">
+      <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      <Chatbot 
+      config={config}
+      messageParser={MessageParser}
+      actionProvider={ActionProvider}
+      />        
+      </header>
+    </div>
+  );
 }
-
 export default App;
